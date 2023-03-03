82b4a96b43212cb9d71f712bdf9df4b7dacd1ccd
EC2 Default User <ec2-user@ip-172-31-30-35.ec2.internal>
2023-03-03 07:15:18 +0000

    pushing to docker hub

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=okellom/udacity-ml-microservice
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker --tag udacity-ml-microservice $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
+
