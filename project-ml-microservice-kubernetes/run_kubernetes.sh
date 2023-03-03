af9d308e2525c3fc8eae178f22ec189f76a4ecaf
EC2 Default User <ec2-user@ip-172-31-30-35.ec2.internal>
2023-03-03 21:37:43 +0000

    deploy with kubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=okellom/udacity-ml-microservice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacity-ml-microservice\
+	--image=$dockerpath\
+	--port=80 --labels app=udacity-ml-microservice
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udacity-ml-microservice 8000:80
+
 
